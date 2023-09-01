dbe0fa238be7393396dff92cd7e05bba20bca9ab
TejaRamoju <ramoju.teja@jktech.com>
2023-09-01 20:03:29 +0530

    Latest Changes  - 01/09

--- src/app/mat-dialog2/mat-dialog2.component.ts
+++ src/app/mat-dialog2/mat-dialog2.component.ts
@@ -15,22 +15,22 @@ export class MatDialog2Component implements OnInit {
   @ViewChild('paginator2') paginator2: MatPaginator;
 
 
-  displayedColumns1: string[] = ['Class Description', 'Sub Department Description', 'Sub Class Description', 'SKU Description', 'SKU Id'];
-  displayedColumns2: string[] = ['DIVISION_NAME', 'DEPT_NAME',"CLASS_NAME", "SUBCLASS_NAME", 'SKU Type', 'SKU_NAME', 'SKU_ID'];
+  displayedColumns1: any;
+  displayedColumns2: any;
 
   dataSource1: MatTableDataSource<any>
   dataSource2: MatTableDataSource<any>
 
-  selectedColumn1:any;
-  selectedColumn2:any;
+  selectedColumn1: any;
+  selectedColumn2: any;
 
-  indexNumber:number = 0;
+  indexNumber: number = 0;
 
 
   list1: any = []
   list2: any = []
 
-  constructor(private apiService: ApiService, private router:Router) {
+  constructor(private apiService: ApiService, private router: Router) {
 
   }
   ngOnInit() {
@@ -41,14 +41,19 @@ export class MatDialog2Component implements OnInit {
 
 
   getTableData() {
-    let body = {}
+    let body = {
+      "filename1": sessionStorage.getItem("fileName1"),
+      "filename2": sessionStorage.getItem("fileName2")
+    }
     this.apiService.getRetailTableData(body).subscribe({
       next: (res: any) => {
         this.list1 = JSON.parse(res["List 1"]);
         this.list2 = JSON.parse(res["List 2"]);
         console.log(this.list1, this.list2);
+        this.displayedColumns1 = Object.keys(this.list1[0]).length < 9 ? Object.keys(this.list1[0]) : Object.keys(this.list1[0]).slice(0, 8)
         this.dataSource1 = new MatTableDataSource<any>(this.list1);
         this.dataSource1.paginator = this.paginator1;
+        this.displayedColumns2 = Object.keys(this.list2[0]).length < 9 ? Object.keys(this.list2[0]) : Object.keys(this.list2[0]).slice(0, 8)
         this.dataSource2 = new MatTableDataSource<any>(this.list2);
         this.dataSource2.paginator = this.paginator2;
       }
@@ -57,29 +62,28 @@ export class MatDialog2Component implements OnInit {
 
 
 
-  onChipOptionClick1(data:any){
-    console.log("cLICKED",data);
+  onChipOptionClick1(data: any) {
+    console.log("cLICKED", data);
     this.selectedColumn1 = data;
   }
-  onChipOptionClick2(data:any){
-    console.log("cLICKED",data);
+  onChipOptionClick2(data: any) {
+    console.log("cLICKED", data);
     this.selectedColumn2 = data;
   }
 
-  changeMatTabIndexNo(){
+  changeMatTabIndexNo() {
     this.indexNumber = 1;
   }
 
-  sendColumn(){
+  sendColumn() {
 
-    let body = {
-      "Selected Columns": {"list1":[this.selectedColumn1], "list2":[this.selectedColumn2]}
-    }
+    let body = { "filename1": sessionStorage.getItem("fileName1"), "filename2": sessionStorage.getItem("fileName2"), "Selected Columns": { "list1": [this.selectedColumn1], "list2": [this.selectedColumn2] } }
+
     console.log(body);
 
     this.apiService.sendColumnNames(body).subscribe({
-      next: (res:any) => {
-        console.log("RESS",res);
+      next: (res: any) => {
+        console.log("RESS", res);
         this.apiService.sendColumnRes(res)
         localStorage.setItem("colummRes", JSON.stringify(res.Final_Standardization_Chart))
         localStorage.setItem("colummAllDataRes", JSON.stringify(res))
@@ -90,5 +94,10 @@ export class MatDialog2Component implements OnInit {
   }
 
 
+  getHeadingName(data: any): any {
+    return Object.keys(data)
+  }
+
 
 }
+
